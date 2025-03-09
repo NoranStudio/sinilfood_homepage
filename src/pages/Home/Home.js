9df5e104b993d0bd79811c27f6917f171932bb0a
최은우 <dev92coo@gmail.com>
2025-03-09 16:16:38 +0900

    Temp : 임시 처리

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../../assets/styles/main_section/global.css";
+import "../../assets/styles/main_section/home.css";
 
 /*
     순서대로 Import정렬 및 각 페이지(섹션)마다 css파일이 연결되어있습니다. 
@@ -13,8 +13,6 @@ import News from "./News.js"; // 새소식
 import CS from "./CS.js"; // 고객센터
 import Footer from "../../components/main_section/Footer"; // 푸터
 
-import logo from "../../assets/img/sinil_logo.png";
-import navGray from "../../assets/img/nav_gray.png";
 
 function App() {
   const [isOpenSideMenu, setOpenSideMenu] = useState(false);
@@ -22,76 +20,46 @@ function App() {
     setOpenSideMenu((prev) => !prev);
   };
   return (
-    <div className="App">
+    <div className="home">
       <Header isOpen={onClickToggle} />
-      <MainBanner />
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>1</h1>
+      <h1>2</h1>
+      <h1>2</h1>
+      <h1>2</h1>
+      <h1>2</h1>
+      <h1>3</h1>
+      <h1>3</h1>
+      <h1>3</h1>
+      <h1>3</h1>
+      <h1>4</h1>
+      <h1>4</h1>
+      <h1>4</h1>
+      <h1>4</h1>
+      <h1>5</h1>
+      <h1>5</h1>
+      <h1>5</h1>
+      <h1>5</h1>
+      <h1>6</h1>
+      <h1>6</h1>
+      <h1>6</h1>
+      <h1>6</h1>
+      <h1>7</h1>
+      <h1>7</h1>
+      <h1>7</h1>
+      <h1>7</h1>
+      <h1>8</h1>
+      <h1>8</h1>
+      <h1>8</h1>
+      <h1>8</h1>
+      {/* <MainBanner />
       <Slogan />
       <CenterContents />
       <Slider />
       <News />
-      <CS />
-      {isOpenSideMenu && (
-        <aside>
-          <div className="header-contents">
-            <h1>
-              <a href="/">
-                <img src={logo} alt="Sinilfood Logo" />
-              </a>
-            </h1>
-            <img
-              src={navGray}
-              alt="nav"
-              className="nav-mo"
-              onClick={onClickToggle}
-            />
-          </div>
-          <div className="aside-contents-container">
-            <div className="aside-contents">
-              <p className="aside-contents-title">회사소개</p>
-              <div className="aside-menus">
-                <p className="aside-menu">회사소개</p>
-                <p className="aside-menu">CI</p>
-                <p className="aside-menu">인증 및 허가증</p>
-              </div>
-              <div className="aside-menus">
-                <p className="aside-menu">찾아오시는 길</p>
-                <p className="aside-menu">연혁</p>
-                <p className="aside-menu">조직도</p>
-              </div>
-            </div>
-            <div className="aside-contents">
-              <p className="aside-contents-title">사업영역</p>
-              <div className="aside-menus">
-                <p className="aside-menu">전용유 제작</p>
-                <p className="aside-menu">식용유 종류</p>
-                <p className="aside-menu">신재생에너지 사업</p>
-              </div>
-              <div className="aside-menus">
-                <p className="aside-menu">제조사 현황</p>
-                <p className="aside-menu">신일푸드 물류시스템</p>
-              </div>
-            </div>
-            <div className="aside-contents">
-              <p className="aside-contents-title">제품소개</p>
-              <div className="aside-menus">
-                <p className="aside-menu">자사전용유 | 일반유</p>
-                <p className="aside-menu">프랜차이즈 전용유</p>
-              </div>
-            </div>
-            <div className="aside-contents">
-              <p className="aside-contents-title">고객서비스</p>
-              <div className="aside-menus">
-                <p className="aside-menu">식용유 종류</p>
-                <p className="aside-menu">신재생에너지 사업</p>
-              </div>
-              <div className="aside-menus">
-                <p className="aside-menu">제조사 현황</p>
-                <p className="aside-menu">신일푸드 물류시스템</p>
-              </div>
-            </div>
-          </div>
-        </aside>
-      )}
+      <CS /> */}
     </div>
   );
 }
