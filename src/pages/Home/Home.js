09e0f8ad76f99ecd9194d0156eb79e5429188c28
최은우 <dev92coo@gmail.com>
2025-03-09 17:11:22 +0900

    Fix : 레이아웃 플렉스로 수정

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,65 +1,23 @@
 import React, { useState } from "react";
-import "../../assets/styles/main_section/home.css";
 
-/*
-    순서대로 Import정렬 및 각 페이지(섹션)마다 css파일이 연결되어있습니다. 
- */
-import Header from "../../components/main_section/Header.js"; // 헤더
 import MainBanner from "./MainBanner.js"; // 메인배너
 import Slogan from "./Slogan.js"; // 슬로건
 import CenterContents from "./CenterContents"; // 전국센터, 파트너
 import Slider from "./Slider.js"; // 프렌차이즈
 import News from "./News.js"; // 새소식
 import CS from "./CS.js"; // 고객센터
-import Footer from "../../components/main_section/Footer"; // 푸터
 
+import "../../assets/styles/main_section/home.css";
 
 function App() {
-  const [isOpenSideMenu, setOpenSideMenu] = useState(false);
-  const onClickToggle = () => {
-    setOpenSideMenu((prev) => !prev);
-  };
   return (
     <div className="home">
-      <Header isOpen={onClickToggle} />
-      <h1>1</h1>
-      <h1>1</h1>
-      <h1>1</h1>
-      <h1>1</h1>
-      <h1>2</h1>
-      <h1>2</h1>
-      <h1>2</h1>
-      <h1>2</h1>
-      <h1>3</h1>
-      <h1>3</h1>
-      <h1>3</h1>
-      <h1>3</h1>
-      <h1>4</h1>
-      <h1>4</h1>
-      <h1>4</h1>
-      <h1>4</h1>
-      <h1>5</h1>
-      <h1>5</h1>
-      <h1>5</h1>
-      <h1>5</h1>
-      <h1>6</h1>
-      <h1>6</h1>
-      <h1>6</h1>
-      <h1>6</h1>
-      <h1>7</h1>
-      <h1>7</h1>
-      <h1>7</h1>
-      <h1>7</h1>
-      <h1>8</h1>
-      <h1>8</h1>
-      <h1>8</h1>
-      <h1>8</h1>
-      {/* <MainBanner />
+      <MainBanner />
       <Slogan />
-      <CenterContents />
-      <Slider />
+      {/* <CenterContents /> */}
+      {/* <Slider /> */}
       <News />
-      <CS /> */}
+      <CS />
     </div>
   );
 }
