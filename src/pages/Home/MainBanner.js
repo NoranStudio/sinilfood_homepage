4bcfd593ac38785b0877ec1ea4792e32d048cde3
최은우 <dev92coo@gmail.com>
2025-03-16 17:48:26 +0900

    Feat : 메인 끝난듯

--- src/pages/Home/MainBanner.js
+++ src/pages/Home/MainBanner.js
@@ -2,8 +2,6 @@ import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import shinliFoodImage from "../../assets/img/SHINLI FOOD white.png"; // SHINLI FOOD 이미지
-import phoneIcon from "../../assets/img/phone.png";
-import headsetIcon from "../../assets/img/headset.png";
 import bannerImg1 from "../../assets/img/mainbanner1.png";
 import bannerImg2 from "../../assets/img/mainbanner2.png";
 import bannerImg3 from "../../assets/img/mainbanner3.png";
@@ -24,6 +22,52 @@ const MainBanner = () => {
   const swiperRef = useRef(null);
   const [currentPage, setCurrentPage] = useState(1);
 
+  const bannerData = [
+    {
+      pcImg: bannerImg1,
+      mobileImg: bannerImg1Mo,
+      title: (
+        <>
+          식용유 및 식품 유통 <br />
+          전문기업 <b>신일푸드</b>
+        </>
+      ),
+      description: (
+        <>
+          <b>신일푸드</b> 만의 독자적인 전용 물류 시스템으로 <br />
+          전국적 유통 체인망이 구축되어 있습니다.
+        </>
+      ),
+    },
+    {
+      pcImg: bannerImg2,
+      mobileImg: bannerImg2Mo,
+      title: (
+        <>
+          외식산업의 유지유통 <br />
+          선도기업 <b>신일푸드</b>
+        </>
+      ),
+      description: (
+        <>
+          깨끗하고 믿을 수 있는 식용유 및 푸드 유통기업 <br />
+          신뢰와 품질을 보장하는 <b>신일푸드</b>
+        </>
+      ),
+    },
+    {
+      pcImg: bannerImg3,
+      mobileImg: bannerImg3Mo,
+      title: <>국내 외식산업의 미래와 함께하는 <b>신일푸드</b></>,
+      description: (
+        <>
+          국내 외식산업의 성장과 함께, 풍부한 경험과 <br />
+          독보적인 경쟁력으로 미래를 열어갑니다.
+        </>
+      ),
+    },
+  ];
+
   return (
     <div className="banner-section">
       <div className="banner-contents">
@@ -34,113 +78,43 @@ const MainBanner = () => {
           onSwiper={(swiper) => (swiperRef.current = swiper)}
           onSlideChange={(swiper) => setCurrentPage(swiper.realIndex + 1)}
         >
-          <SwiperSlide>
-            <div className="banner-img-wrapper">
-              <div className="scroll-down-wrapper">
-                <img
-                  src={scrollDown}
-                  alt="스크롤다운이미지"
-                  className="scroll-down-img"
-                />
-              </div>
-              <div className="banner-img-wrapper-mo">
-                <img src={bannerImg1} alt="배너이미지" className="banner-img" />
-                <img
-                  src={bannerImg1Mo}
-                  alt="배너이미지"
-                  className="banner-img-mo"
-                />
-                <div className="banner-texts">
-                  <img src={shinliFoodImage} alt="신일푸드" />
-                  <p className="banner-title">
-                    식용유 및 식품 유통 <br />
-                    전문기업 <b>신일푸드</b>
-                  </p>
-                  <p className="banner-description">
-                    <b>신일푸드</b> 만의 독자적인 전용 물류 시스템으로 <br />
-                    전국적 유통 체인망이 구축되어 있습니다.
-                  </p>
-                  <div className="banner-navigation">
-                    <HiOutlineArrowNarrowLeft
-                      onClick={() => swiperRef.current?.slidePrev()}
+          {bannerData.map((banner, index) => (
+            <SwiperSlide key={index}>
+              <div 
+                className="banner-slide"
+                style={{
+                  "--pc-background": `url(${banner.pcImg})`,
+                  "--mobile-background": `url(${banner.mobileImg})`
+                }}
+              >
+                <div className="banner-content-wrapper">
+                  <div className="scroll-down-wrapper">
+                    <img
+                      src={scrollDown}
+                      alt="스크롤다운이미지"
+                      className="scroll-down-img"
                     />
-                    <b>{currentPage.toString().padStart(2, "0")}</b> <p>03</p>
-                    <HiOutlineArrowNarrowRight
-                      onClick={() => swiperRef.current?.slideNext()}
-                    />
+                  </div>
+                  <div className="banner-texts">
+                    <img src={shinliFoodImage} alt="신일푸드" />
+                    <p className="banner-title">{banner.title}</p>
+                    <p className="banner-description">{banner.description}</p>
+                    <div className="banner-navigation">
+                      <HiOutlineArrowNarrowLeft
+                        onClick={() => swiperRef.current?.slidePrev()}
+                      />
+                      <b>{currentPage.toString().padStart(2, "0")}</b> <p>03</p>
+                      <HiOutlineArrowNarrowRight
+                        onClick={() => swiperRef.current?.slideNext()}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="blank-div" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={bannerImg2} alt="배너이미지" className="banner-img" />
-            <img
-              src={bannerImg2Mo}
-              alt="배너이미지"
-              className="banner-img-mo"
-            />
-            <div className="banner-texts">
-              <img src={shinliFoodImage} alt="신일푸드" />
-              <p className="banner-title">
-                외식산업의 유지유통 <br />
-                선도기업 <b>신일푸드</b>
-              </p>
-              <p className="banner-description">
-                깨끗하고 믿을 수 있는 식용유 및 푸드 유통기업 <br />
-                신뢰와 품질을 보장하는 <b>신일푸드</b>
-              </p>
-              <div className="banner-navigation">
-                <HiOutlineArrowNarrowLeft
-                  onClick={() => swiperRef.current?.slidePrev()}
-                />
-                <b>{currentPage.toString().padStart(2, "0")}</b> <p>03</p>
-                <HiOutlineArrowNarrowRight
-                  onClick={() => swiperRef.current?.slideNext()}
-                />
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={bannerImg3} alt="배너이미지" className="banner-img" />
-            <img
-              src={bannerImg3Mo}
-              alt="배너이미지"
-              className="banner-img-mo"
-            />
-            <div className="banner-texts">
-              <img src={shinliFoodImage} alt="신일푸드" />
-              <p className="banner-title">
-                국내 외식산업의 미래와 함께하는 <b>신일푸드</b>
-              </p>
-              <p className="banner-description">
-                국내 외식산업의 성장과 함께, 풍부한 경험과 <br />
-                독보적인 경쟁력으로 미래를 열어갑니다.
-              </p>
-              <div className="banner-navigation">
-                <HiOutlineArrowNarrowLeft
-                  onClick={() => swiperRef.current?.slidePrev()}
-                />
-                <b>{currentPage.toString().padStart(2, "0")}</b> <p>03</p>
-                <HiOutlineArrowNarrowRight
-                  onClick={() => swiperRef.current?.slideNext()}
-                />
-              </div>
-            </div>
-          </SwiperSlide>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
-
-      {/* 우측 사이드 버튼 */}
-      {/* <div className="side-buttons">
-        <a href="#contact" className="contact-button">
-          <img src={phoneIcon} alt="Phone Icon" className="icon-image" />
-        </a>
-        <a href="#contact" className="contact-button">
-          <img src={headsetIcon} alt="Headset Icon" className="icon-image" />
-        </a>
-      </div> */}
     </div>
   );
 };
