8056f5ee1ecb79477220e017474b08898957937e
최은우 <dev92coo@gmail.com>
2025-03-16 14:36:31 +0900

    Fix : footer 경로 수정정

--- src/components/baseLayout/BaseLayout.jsx
+++ src/components/baseLayout/BaseLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Footer from "./Footer";
+import Footer from "../footer";
 import Header from "../header";
 const BaseLayout = () => (
   <div className="base-layout">
