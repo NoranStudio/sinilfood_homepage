b8a0a9cd2fee253ddb51198be6bbeaad483817e7
최은우 <dev92coo@gmail.com>
2025-03-16 18:17:29 +0900

    Feat: 슬로건 처리 완료.

--- src/pages/Home/Slogan.js
+++ src/pages/Home/Slogan.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -13,11 +13,25 @@ import Arrow from "../../assets/img/slogan-arrow.png";
 const Slogan = () => {
   const swiper1Ref = useRef(null); // 첫 번째 Swiper : 메인
   const swiper2Ref = useRef(null); // 두 번째 Swiper : 서브
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [isTablet, setIsTablet] = useState(window.innerWidth <= 1024 && window.innerWidth > 768);
 
   const handleNext = () => {
     swiper1Ref.current?.slideNext();
     swiper2Ref.current?.slideNext();
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+      setIsTablet(window.innerWidth <= 1024 && window.innerWidth > 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div className="slogan-section">
@@ -36,88 +50,94 @@ const Slogan = () => {
         엄선된 품질과 철저한 관리를 바탕으로 최상의 <br className="mo-br" />
         제품만을 제공합니다.
       </p>
-      <div className="slogan-contents-wrapper">
-        <Swiper
-          spaceBetween={0}
-          slidesPerView={1}
-          loop={true}
-          onSwiper={(swiper) => (swiper1Ref.current = swiper)}
-        >
-          <SwiperSlide>
+      {!isMobile && (
+        <div className="slogan-contents-wrapper">
+          <Swiper
+            spaceBetween={0}
+            slidesPerView={1}
+            loop={true}
+            onSwiper={(swiper) => (swiper1Ref.current = swiper)}
+          >
+            <SwiperSlide>
+              <img src={sloganMainBanner1} alt="메인배너1" />
+              <div className="slogan-banner-contents">
+                <div className="textbox">
+                  <p className="slogan-banner-title">FC 전용유 제작</p>
+                  <p className="slogan-banner-description">
+                    프랜차이즈 전용 FC 식용유
+                    <br />
+                    일관된 맛과 품질로 고객에게 늘 한결같은 맛을 제공하세요
+                  </p>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={sloganMainBanner2} alt="메인배너2" />
+              <div className="slogan-banner-contents">
+                <div className="textbox">
+                  <p className="slogan-banner-title">업소용 식용유</p>
+                  <p className="slogan-banner-description">
+                    업소 전용 프리미엄 식용유
+                    <br />
+                    고온에서도 안정적인 품질로 튀김의 바삭함과 풍미를 오래
+                    유지합니다
+                  </p>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+          {!isTablet && (
+            <Swiper
+              spaceBetween={0}
+              slidesPerView={1}
+              loop={true}
+              onSwiper={(swiper) => (swiper2Ref.current = swiper)}
+              className="slogan-subbanner"
+            >
+              <SwiperSlide>
+                <img src={sloganSubBanner1} alt="서브배너1" />
+                <div className="slogan-banner-contents"></div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={sloganSubBanner2} alt="서브배너2" />
+                <div className="slogan-banner-contents"></div>
+              </SwiperSlide>
+            </Swiper>
+          )}
+          <div className="sloganbanner-navigation">
+            <img src={Arrow} alt="arrow" onClick={handleNext} />
+          </div>
+        </div>
+      )}
+      {isMobile && (
+        <div className="slogan-contents-wrapper-mo">
+          <div className="contents">
             <img src={sloganMainBanner1} alt="메인배너1" />
-            <div className="slogan-banner-contents">
-              <div className="textbox">
-                <p className="slogan-banner-title">FC 전용유 제작</p>
-                <p className="slogan-banner-description">
-                  프랜차이즈 전용 FC 식용유
-                  <br />
-                  일관된 맛과 품질로 고객에게 늘 한결같은 맛을 제공하세요
-                </p>
-              </div>
+            <div className="contents-text">
+              <p className="title">FC 전용유 제작</p>
+              <p className="slogan-contents-mo-description">
+                프랜차이즈 전용 FC 식용유
+                <br />
+                일관된 맛과 품질로 고객에게
+                <br />늘 한결같은 맛을 제공하세요
+              </p>
             </div>
-          </SwiperSlide>
-          <SwiperSlide>
+          </div>
+          <div className="contents">
             <img src={sloganMainBanner2} alt="메인배너2" />
-            <div className="slogan-banner-contents">
-              <div className="textbox">
-                <p className="slogan-banner-title">업소용 식용유</p>
-                <p className="slogan-banner-description">
-                  업소 전용 프리미엄 식용유
-                  <br />
-                  고온에서도 안정적인 품질로 튀김의 바삭함과 풍미를 오래
-                  유지합니다
-                </p>
-              </div>
+            <div className="contents-text">
+              <p className="title">업소용 식용유</p>
+              <p className="slogan-contents-mo-description">
+                업소 전용 프리미엄 식용유
+                <br />
+                고온에서도 안정적인 품질로 튀김의 바삭함과
+                <br />
+                풍미를 오래 유지합니다
+              </p>
             </div>
-          </SwiperSlide>
-        </Swiper>
-        <Swiper
-          spaceBetween={0}
-          slidesPerView={1}
-          loop={true}
-          onSwiper={(swiper) => (swiper2Ref.current = swiper)}
-          className="slogan-subbanner"
-        >
-          <SwiperSlide>
-            <img src={sloganSubBanner1} alt="서브배너1" />
-            <div className="slogan-banner-contents"></div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={sloganSubBanner2} alt="서브배너2" />
-            <div className="slogan-banner-contents"></div>
-          </SwiperSlide>
-        </Swiper>
-        <div className="sloganbanner-navigation">
-          <img src={Arrow} alt="arrow" onClick={handleNext} />
-        </div>
-      </div>
-      <div className="slogan-contents-wrapper-mo">
-        <div className="contents">
-          <img src={sloganMainBanner1} alt="메인배너1" />
-          <div className="contents-text">
-            <p className="title">FC 전용유 제작</p>
-            <p className="slogan-contents-mo-description">
-              프랜차이즈 전용 FC 식용유
-              <br />
-              일관된 맛과 품질로 고객에게
-              <br />늘 한결같은 맛을 제공하세요
-            </p>
           </div>
         </div>
-        <div className="contents">
-          <img src={sloganMainBanner2} alt="메인배너2" />
-          <div className="contents-text">
-            <p className="title">업소용 식용유</p>
-            <p className="slogan-contents-mo-description">
-              업소 전용 프리미엄 식용유
-              <br />
-              고온에서도 안정적인 품질로 튀김의 바삭함과
-              <br />
-              풍미를 오래 유지합니다
-            </p>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
