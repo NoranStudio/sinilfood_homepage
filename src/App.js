7200c90840d98a9f68790fd20cc22cf49797026a
최은우 <dev92coo@gmail.com>
2025-03-30 21:47:56 +0900

    Feat: path 변경시 최상단으로 이동하도록.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppRoutes from "./routes";
 import "./App.css";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
+      <ScrollToTop />
       <AppRoutes />
     </div>
   );
 };
 
 export default App;
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+      window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
