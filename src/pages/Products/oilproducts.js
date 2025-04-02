19c8c510bbe0eecabd3bf527c3e08cb7968bb148
최은우 <dev92coo@gmail.com>
2025-04-02 23:39:07 +0900

    Feat :  네비바 수정.

--- src/pages/Products/oilproducts.js
+++ src/pages/Products/oilproducts.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 //import Footer from '../../components/Footer';
 // 자사 전용유 / 일반유 섹션 컴포넌트 임포트
 import SelfOil from "../../components/oilproduct/selfoil";
@@ -6,9 +6,13 @@ import GeneralOil from "../../components/oilproduct/generaloil";
 import OilproductNavbar from "../../components/oilproduct/OilproductNavbar";
 import OilproductBanner from "../../components/oilproduct/OilproductBanner";
 import "./op.css"; // 페이지 전용 스타일
-
+import { useLocation } from "react-router-dom";
 function OilProducts() {
-  const [activeTab, setActiveTab] = useState("selfoil");
+  const { state } = useLocation();
+  const [activeTab, setActiveTab] = useState(state?.subMenu || "");
+  useEffect(() => {
+    setActiveTab(state?.subMenu || "");
+  }, [state?.subMenu]);
 
   const renderContent = () => {
     switch (activeTab) {
